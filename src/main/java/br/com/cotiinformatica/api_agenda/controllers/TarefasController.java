e4e25d57bbafc34950c91df5de4bc977141b9d3e
beatrizlima-tech <bealima.tech@gmail.com>
2026-03-23 13:11:55 -0300

    feat: adiciona consulta de tarefas por data e exclusão com integração entre controller, service e repository

--- src/main/java/br/com/cotiinformatica/api_agenda/controllers/TarefasController.java
+++ src/main/java/br/com/cotiinformatica/api_agenda/controllers/TarefasController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tarefas")
@@ -22,19 +24,29 @@ public class TarefasController {
         return ResponseEntity.ok().body(response);
     }
 
-    @PutMapping
-    public ResponseEntity<?> put() {
-        return ResponseEntity.ok().build();
+    @PutMapping("{id}")
+    public ResponseEntity<?> put(@PathVariable UUID id, @RequestBody TarefaRequest request) {
+
+        var response = tarefaService.atualizar(id, request);
+        return ResponseEntity.ok().body(response);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> delete() {
-        return ResponseEntity.ok().build();
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+
+        var response = tarefaService.excluir(id);
+        return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping
-    public ResponseEntity<?> get() {
-        var response = tarefaService.consultar();
+    @GetMapping("{dataMin}/{dataMax}")
+    public ResponseEntity<?> get(@PathVariable LocalDate dataMin, @PathVariable LocalDate dataMax) {
+        var response = tarefaService.consultarPorDatas(dataMin, dataMax);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getById(@PathVariable UUID id) {
+        var response = tarefaService.obterPorId(id);
         return ResponseEntity.ok().body(response);
     }
 }
